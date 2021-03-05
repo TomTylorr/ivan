34cf16d579bad6fbb86073b594fe6d20b0b6d2bd
tomtylorr <tomtylorr@gmail.com>
2021-03-05 21:51:13 +0300

    init: the first version of site

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// console.log("Hello, World!");
+// alert("Hello, World!");
+
+// let dfdf = 12;
+
+// console.log(dfdf);
+
+
+// let leftBorderWidth = "Hello World!";
+
+// console.log(leftBorderWidth);
+
+// console.log(typeof("" + false));
+// console.log(typeof(+'5'));
+// console.log(5 + +'5');
+
+console.log(typeof(prompt("how old are you?", "18")));
